68b18d7ac83c86c5d38e2496adb8c2bfd1a6518c
Edcarlo Lima <edcarlolimsan@gmail.com>
2021-09-02 22:04:47 -0300

    Introdução aos códigos JS

---
+++ mat.js
@@ -0,0 +1,8 @@
+let numero1 = 10;
+let numero2 = 20;
+
+function somar(n1,n2) {
+    return n1 + n2;
+}
+
+console.log(somar(numero1,numero2));
---
+++ nome.js
@@ -0,0 +1,5 @@
+let nome = 'Edcarlo';
+let sobreNome = 'Lima';
+let curso = 'Node.js';
+
+console.log('Dev '+nome+' '+sobreNome+' iniciando com '+curso);
